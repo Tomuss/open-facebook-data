8e870b32e7ecbfb5258c89a372c2a27821263285
tomus <tomus@Philomen>
2021-05-11 18:17:22 +0200

    Commit temporaire pour test.

--- routes/users.js
+++ routes/users.js
@@ -24,6 +24,7 @@ router.get('/profile',
                 })
                 .then(function (response) {
                     page.events = response.data.data;
+                    console.log(response.data.data);
                 })
                 .catch(function (error) {
                     console.log(`Erreur lors de la récupération des données facebook: ${error}`);
