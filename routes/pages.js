52075ffb2de1246c631cfe5ff906001cb77fb32e
tomus <tomus@Philomen>
2021-05-29 15:46:15 +0200

    Ajout de nouvelles informations concernant les événements pour l'API des pages.

--- routes/pages.js
+++ routes/pages.js
@@ -50,7 +50,7 @@ router.get('/:id', function(req, res, next){
                 var events = response.data.data;
                 var eventRequest = [];
                 events.forEach(function (event){
-                    eventRequest.push({method: 'GET', relative_url: event.id+ '?fields=id,name,start_time,end_time,cover,owner,parent_group'});
+                    eventRequest.push({method: 'GET', relative_url: event.id+ '?fields=id,name,start_time,end_time,place,cover,owner,parent_group'});
                 });
                 var parms = {
                         batch : JSON.stringify(eventRequest),
